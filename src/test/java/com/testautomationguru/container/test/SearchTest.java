fe7f7f8dcf54285b7303cba752df6290ccb65db5
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-01-19 17:24:35 +0530

    Update SearchTest.java

--- src/test/java/com/testautomationguru/container/test/SearchTest.java
+++ src/test/java/com/testautomationguru/container/test/SearchTest.java
@@ -1,9 +1,9 @@
 package com.testautomationguru.container.test;
 
 import java.net.MalformedURLException;
-import org.testng.Assert;
+import org.testng.Assert.*;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest.*;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import com.testautomationguru.container.pages.SearchPage;
