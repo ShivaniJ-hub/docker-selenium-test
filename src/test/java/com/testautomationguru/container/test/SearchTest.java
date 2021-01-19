29675b048fcb64097ed7e02131a2a5904bdda79d
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-01-19 17:23:29 +0530

    Update SearchTest.java

--- src/test/java/com/testautomationguru/container/test/SearchTest.java
+++ src/test/java/com/testautomationguru/container/test/SearchTest.java
@@ -1,10 +1,9 @@
 package com.testautomationguru.container.test;
 
 import java.net.MalformedURLException;
-import org.testNG.assert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.AfterTest.*;
 import org.testng.annotations.Test;
 
 import com.testautomationguru.container.pages.SearchPage;
@@ -19,11 +18,16 @@ public class SearchTest extends BaseTest {
         google = new SearchPage(driver);
     }
 
-    @Test()
+    @Test
     public void googleTest() {
         google.goTo();
         Assert.assertTrue(true);
     }
+    @AfterTest
+    public void close_browser(){
+       driver.close();
+       driver.quit();
+    } 
    
 
 }
